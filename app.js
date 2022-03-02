b160dbe5e79f3ad6703edc776651750419cf8840
progmamun <mamun333089@gmail.com>
2022-03-02 16:56:13 +0600

    update

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 'use strict';
 
-const API_KEY = `638276579b54c6cdb0de136de52c2951`;
+// const API_KEY = `638276579b54c6cdb0de136de52c2951`;
+const API_KEY = `2e81c84cc474ebf04ee39c84e1fc97cc`;
 const searchTemperature = () => {
   const city = document.getElementById('city-name').value;
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid={API_KEY}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid={API_KEY}&units=metric`;
   // console.log(url);
   fetch(url)
     .then(res => res.json())
     .then(data => displayTemperature(data));
 };
 
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
+
 const displayTemperature = temperature => {
+  setInnerText('city', temperature.name);
+  setInnerText('temperature', temperature.main.temp);
   console.log(temperature);
 };
